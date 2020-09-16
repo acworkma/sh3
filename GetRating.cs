6b23f54bb3bd35bacf41ec1b187716a944bad79d
Adam Workman <adam@Taz.local>
2020-09-16 13:15:15 -0600

    Cosmos1

--- GetRating.cs
+++ GetRating.cs
@@ -7,29 +7,35 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Azure.Documents.SystemFunctions;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace my.GetRating
+namespace Functions
 {
-    public static class GetRating
+    public class GetRatings
     {
-        [FunctionName("GetRating")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        [FunctionName("GetRatings")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "GetRatings/{userId}")] HttpRequest req,
+                        [CosmosDB(
+                            databaseName: "IceCream",
+                            collectionName: "Ratings",
+                            ConnectionStringSetting = "CosmosDBConnection",
+                            SqlQuery = "select * from Ratings r where r.userId = {userId}",
+                            PartitionKey = "/id")]
+                            IEnumerable<CreateRatingResponse> ratings, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            if (ratings == null || !ratings.Any())
+            {
+                return new NotFoundResult();
+            }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(ratings);
         }
     }
 }
